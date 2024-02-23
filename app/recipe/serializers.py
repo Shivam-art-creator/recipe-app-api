29b0a24b5d2eb9f9c3b3c4c4f6f116b683b14a84
shivam <shivamkeshri3580@gmail.com>
2024-02-23 11:04:33 +0530

    Build Recipe API done

---
+++ app/recipe/serializers.py
@@ -0,0 +1,23 @@
+"""
+SErializers for recipe APIs
+"""
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """SErializer for recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'title', 'time_minutes', 'price', 'link')
+        read_only_fileds = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ('description',)
